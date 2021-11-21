a9da44de0d20539fdb888421f8046defc2429b52
Toby Nguyen <toby_nguyen6@yahoo.com>
2021-11-21 10:46:26 -0800

    Criteria and Prompts

--- develop/script.js
+++ develop/script.js
@@ -1,56 +1,98 @@
-// Assignment code here
-let generateBtnEl = document.getElementById('generate');
+// Special Characters
+let specialCharacters = "!@#$%^&*()";
+let generateButton = document.getElementById('generate')
+generateButton.addEventListener('click', writePassword)
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-let totalPassword = "";
-let inputLowercase = true;
-let inputNumeric = true;
-
-
-let special = "!@#$%^&*"
-let numeric = "1234567890"
-let lowercase = "abc";
-let uppercase = "ABC";
-
-if(inputLowercase){
-  totalPassword += lowercase;
-}
-
-if(inputNumeric){
-  totalPassword += numeric;
-}
-
-for(let i = 0; i <  length; i++){
-  
-}
-
-// Write password to the #password input
+// Password for the #password input 
 function writePassword() {
-  //var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  var passwordLength = window.prompt("What is the length of your password?");
-    if (passwordLength < 8){
-      window.alert("Your password must be at least 8 characters and no more than 128 characters.")
-      return null
-    }
-    if (passwordLength > 128){
-      window.alert("Your password must be at least 8 characters and no more than 128 characters.")
-      return null
-    }
-  var passwordLowercase = window.confirm("Does your password include lowercase characters? Click Ok for yes or Click Cancel for no");
-  var passwordUppercase = window.confirm("Does your password include uppercase characters? Click Ok for yes or Click Cancel for no");
-  var passwordNumeric = window.confirm("Does your password include numeric characters? Click Ok for yes or Click Cancel for no" );
-  var passwordSpecial = window.confirm("Does your password include special characters? Click Ok for yes or Click Cancel for no");
-    if (passwordLowercase === false && passwordUppercase === false && passwordNumeric === false && passwordSpecial === false){
-      window.alert("You must have at least one character type selected for your password")
-      return null
-    }
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
 
 }
 
-// Add event listener to generate button
-generateBtnEl.addEventListener("click", writePassword) 
+// Password Criteria Prompts 
+function generatePassword() {
+    var passwordLength = window.prompt("What is the length of your desired password? It must be no less than 8 but no more than 120 characters.");
+
+    var numbers = window.confirm("Does your password include numbers?");
+
+    var lowerCases = window.confirm("Does your password have any lowercase characters?");
+
+    var upperCases = window.confirm("Does your password have any uppercase characters?");
+
+    var special = window.confirm("Does your password have any special characters?");
+
+    var minimumCount = 0; 
+
+    var minimumNumbers = "";
+    var minimumLowerCases = "";
+    var minimumUpperCases = "";
+    var minimumSpecialCharacters = "";
+
+    // Function Array 
+    var functionArray = {
+        getNumbers: function() {
+            return String.fromCharCode(Math.floor(Math.random() * 10 + 48))
+        },
+
+        getLowerCases: function() {
+            return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
+        },
+
+        getUpperCases: function() {
+            return String.fromCharCode(Math.floor(Math.random()* 26 + 65));
+        },
+
+        getSpecialCharacters: function() {
+            return specialCharacters[Math.floor(Math.random() * specialCharacters.length)]
+        }
+
+
+    };
+
+    // If user Selects OK for all. 
+    if (numbers === true) {
+        minimumNumbers = functionArray.getNumbers();
+        minimumCount++;
+    }
+
+    if (lowerCases === true) {
+        minimumLowerCases = functionArray.getLowerCases();
+        minimumCount++;
+    }
+
+    if (upperCases === true) {
+        minimumUpperCases = functionArray.getUpperCases();
+        minimumCount++;
+    }
+
+    if (special === true) {
+        minimumSpecialCharacters = functionArray.getSpecialCharacters();
+        minimumCount++;
+    }
+
+    if (lowerCases === false && upperCases === false && numbers === false && special === false){
+        window.alert("You must have at least one character type selected for your password")
+        return null
+      }
+
+    var randomPasswordGenerated = "";
+
+    // loop to obtain random password 
+    for (let i =0; i< (parseInt(passwordLength) - minimumCount); i++){
+        var randomNumberPicked = Math.floor(Math.random() * 4);
+
+        randomPasswordGenerated += randomNumberPicked;
+    }
+
+    // random password's with our minimums to ensure characters are added 
+    randomPasswordGenerated += minimumNumbers;
+    randomPasswordGenerated += minimumLowerCases;
+    randomPasswordGenerated += minimumUpperCases;
+    randomPasswordGenerated += minimumSpecialCharacters;
+
+    return randomPasswordGenerated;
+
+}
